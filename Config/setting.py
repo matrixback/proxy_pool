9f60e2ab106204541e85af11617c3385fcebde2d
Matrix <matrix32768@163.com>
2019-02-25 20:37:08 +0800

    update: docker-compose 文件修改redis

--- Config/setting.py
+++ Config/setting.py
@@ -16,8 +16,8 @@
 DATABASES = {
     "default": {
         "TYPE": "SSDB",  # TYPE SSDB/MONGODB if use redis, only modify the host port, the type should be SSDB
-        "HOST": "127.0.0.1",
-        "PORT": 8888,
+        "HOST": "proxy_redis",
+        "PORT": 6379,
         "NAME": "proxy",
         "PASSWORD": ""
 
